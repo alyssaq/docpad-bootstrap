7d92eaa1818b10791afd5f2d118c7fd37ffe8bb1
alyssaq <alyssaquek@gmail.com>
2013-02-21 19:37:22 +1100

    More canvas stuff and cleaning up coffeescript

--- out/scripts/script.js
+++ out/scripts/script.js
@@ -1,11 +1,20 @@
 (function() {
-  var App, addEventHandler, canvasDrop, canvasDrop2, drawLine, drop, finishDrawing, getPosition, initialize, loadCanvasAndImg, stopDefault, to_image;
+  var App, addEventHandler, clearBtn, drawLine, drawLines, drop, finishDrawings, getPosition, initialize, loadCanvasAndImg, stopDefault;
 
   App = App || {
     Globals: {
-      foreGcolour: '#0044cc',
-      backGcolour: '#000000'
+      foreGcolour: $("#btnForeG").css('backgroundColor'),
+      backGcolour: $("#btnBackG").css('backgroundColor')
     }
+  };
+
+  clearBtn = function(resCtx) {
+    $("#drop").empty();
+    $("#imageInfo").html('');
+    resCtx.save();
+    resCtx.setTransform(1, 0, 0, 1, 0, 0);
+    resCtx.clearRect(0, 0, resCtx.canvas.width, resCtx.canvas.height);
+    return resCtx.restore();
   };
 
   getPosition = function(mouseEvent, element) {
@@ -25,164 +34,77 @@
     };
   };
 
-  canvasDrop2 = function(context) {
-    return function(e) {
-      var dropBoxElem, file, files, formData, i, info, reader, _i, _len;
-      e = e || window.event;
-      stopDefault(e);
-      files = e.dataTransfer.files;
-      info = "";
-      formData = new FormData();
-      dropBoxElem = e.currentTarget;
-      for (i = _i = 0, _len = files.length; _i < _len; i = ++_i) {
-        file = files[i];
-        info += 'Name: ' + file.name + '<br/>Size: ' + file.size + ' bytes<br/>';
-        formData.append('file', file);
-        reader = new FileReader();
-        document.getElementById("drawingCanvas").width = 2000;
-        reader.onload = function(event) {
-          var image;
-          image = new Image();
-          image.src = event.target.result;
-          return context.drawImage(image, 0, 0);
-        };
-        reader.readAsDataURL(file);
-      }
-      $("#imageInfo").html(info);
-      return false;
-    };
-  };
-
-  canvasDrop = function(context) {
-    return function(e) {
-      var file, files, i, image, info, _i, _len;
-      e = e || window.event;
-      stopDefault(e);
-      files = e.dataTransfer.files;
-      info = "";
-      for (i = _i = 0, _len = files.length; _i < _len; i = ++_i) {
-        file = files[i];
-        info += 'Name: ' + file.name + '<br/>Size: ' + file.size + ' bytes<br/>';
-        image = new Image();
-        image.onload = (function(image, i) {
-          return function(e) {
-            var canvas;
-            canvas = document.getElementById("drawingCanvas");
-            canvas.width = image.width;
-            canvas.height = image.height;
-            context = canvas.getContext("2d");
-            context.drawImage(image, 0, 0);
-            return window.URL.revokeObjectURL(this.src);
-          };
-        })(image, i);
-        image.src = window.URL.createObjectURL(file);
-        context.drawImage(image, 0, 0);
-        image = null;
-      }
-      $("#imageInfo").html(info);
-      return false;
-    };
-  };
-
-  initialize = function(canvasId) {
-    var context, context2, element, element2;
-    element = document.getElementById(canvasId);
-    context = element.getContext("2d");
-    context.strokeStyle = App.Globals.foreGcolour;
-    context.lineWidth = $("#btnLinewidth").val();
-    context.lineJoin = "round";
-    context.lineCap = "round";
-    if (canvasId === "drawingCanvas") {
-      element.addEventListener('dragenter', stopDefault, false);
-      element.addEventListener('dragover', stopDefault, false);
-      element.addEventListener('drop', canvasDrop(context), false);
-    }
-    element2 = document.getElementById("resultCanvas");
-    context2 = element2.getContext("2d");
-    element2.width = element.width;
-    element2.height = element.height;
-    context2.strokeStyle = "white";
-    context2.lineWidth = $("#btnLinewidth").val();
-    context2.lineJoin = "round";
-    context2.lineCap = "round";
-    context2.fillStyle = "lightgrey";
-    context2.fillRect(0, 0, element.width, element.height);
-    $(element).mousedown(function(mouseEvent) {
-      var position;
-      position = getPosition(mouseEvent, element);
-      context.moveTo(position.X, position.Y);
-      context.beginPath();
-      context2.moveTo(position.X, position.Y);
-      context2.beginPath();
-      return $(this).mousemove(function(mouseEvent) {
-        position = getPosition(mouseEvent, element);
-        drawLine(mouseEvent, element, context, position);
-        return drawLine(mouseEvent, element2, context2, position);
-      }).mouseup(function(mouseEvent) {
-        position = getPosition(mouseEvent, element);
-        drawLine(mouseEvent, element, context, position);
-        drawLine(mouseEvent, element2, context2, position);
-        finishDrawing(mouseEvent, element, context);
-        return finishDrawing(mouseEvent, element2, context2);
-      }).mouseout(function(mouseEvent) {
-        position = getPosition(mouseEvent, element);
-        drawLine(mouseEvent, element, context, position);
-        drawLine(mouseEvent, element2, context2, position);
-        finishDrawing(mouseEvent, element, context);
-        return finishDrawing(mouseEvent, element2, context2);
-      });
-    });
-    $("#btnClear").click(function() {
-      var currentWidth, resCtx;
-      currentWidth = context.lineWidth;
-      element.width = element.width;
-      context.lineWidth = currentWidth;
-      $("#imageInfo").html('');
-      resCtx = document.getElementById("resultCanvas").getContext("2d");
-      resCtx.save();
-      resCtx.setTransform(1, 0, 0, 1, 0, 0);
-      resCtx.clearRect(0, 0, resCtx.canvas.width, resCtx.canvas.height);
-      return resCtx.restore();
-    });
-    $("#btnForeG").click(function() {
-      context.globalCompositeOperation = "source-over";
-      context.strokeStyle = App.Globals.foreGcolour;
-      return context2.strokeStyle = "white";
-    });
-    $("#btnBackG").click(function() {
-      context.globalCompositeOperation = "source-over";
-      context.strokeStyle = App.Globals.backGcolour;
-      return context2.strokeStyle = "black";
-    });
-    $("#btnEraser").click(function() {
-      context.globalCompositeOperation = "destination-out";
-      context.strokeStyle = "rgba(0,0,0,1.0)";
-      return context2.strokeStyle = "lightgrey";
-    });
-    return $("#btnLinewidth").change(function(event) {
-      context.lineWidth = $(this).val();
-      return context2.lineWidth = $(this).val();
-    });
-  };
-
-  drawLine = function(mouseEvent, element, context, position) {
+  drawLine = function(context, position) {
     context.lineTo(position.X, position.Y);
     return context.stroke();
   };
 
-  finishDrawing = function(mouseEvent, element, context) {
-    context.closePath();
-    return $(element).unbind("mousemove").unbind("mouseup").unbind("mouseout");
+  drawLines = function(srcCtx, resCtx, position) {
+    drawLine(srcCtx, position);
+    return drawLine(resCtx, position);
   };
 
-  $(document).ready(function() {
-    if (!Modernizr.canvas) {
-      $("#message").html("<p><b>WARNING</b>: Your browser does not support HTML5's canvas feature, you won't be able to see the drawings below</p>");
-      return $("article").hide();
-    } else {
+  finishDrawings = function(srcCtx, resCtx, position) {
+    drawLines(srcCtx, resCtx, position);
+    srcCtx.closePath();
+    resCtx.closePath();
+    return $(srcCtx.canvas).unbind("mousemove").unbind("mouseup").unbind("mouseout");
+  };
 
-    }
-  });
+  initialize = function(srcCanvas) {
+    var resCanvas, resCtx, srcCtx;
+    srcCtx = srcCanvas.getContext("2d");
+    srcCtx.strokeStyle = App.Globals.foreGcolour;
+    resCanvas = document.getElementById("resultCanvas");
+    resCtx = resCanvas.getContext("2d");
+    resCanvas.width = srcCanvas.width;
+    resCanvas.height = srcCanvas.height;
+    srcCtx.lineWidth = resCtx.lineWidth = $("#btnLinewidth").val();
+    srcCtx.lineJoin = resCtx.lineJoin = "round";
+    srcCtx.lineCap = resCtx.lineCap = "round";
+    resCtx.strokeStyle = "white";
+    resCtx.fillStyle = "lightgrey";
+    resCtx.fillRect(0, 0, srcCanvas.width, srcCanvas.height);
+    $(srcCanvas).mousedown(function(mouseEvent) {
+      var position;
+      position = getPosition(mouseEvent, srcCanvas);
+      srcCtx.moveTo(position.X, position.Y);
+      srcCtx.beginPath();
+      resCtx.moveTo(position.X, position.Y);
+      resCtx.beginPath();
+      return $(this).mousemove(function(mouseEvent) {
+        position = getPosition(mouseEvent, srcCanvas);
+        return drawLines(srcCtx, resCtx, position);
+      }).mouseup(function(mouseEvent) {
+        position = getPosition(mouseEvent, srcCanvas);
+        return finishDrawings(srcCtx, resCtx, position);
+      }).mouseout(function(mouseEvent) {
+        position = getPosition(mouseEvent, srcCanvas);
+        return finishDrawings(srcCtx, resCtx, position);
+      });
+    });
+    $("#btnClear").click(function() {
+      return clearBtn(resCtx);
+    });
+    $("#btnForeG").click(function() {
+      srcCtx.globalCompositeOperation = "source-over";
+      srcCtx.strokeStyle = App.Globals.foreGcolour;
+      return resCtx.strokeStyle = "white";
+    });
+    $("#btnBackG").click(function() {
+      srcCtx.globalCompositeOperation = "source-over";
+      srcCtx.strokeStyle = App.Globals.backGcolour;
+      return resCtx.strokeStyle = "black";
+    });
+    $("#btnEraser").click(function() {
+      srcCtx.globalCompositeOperation = "destination-out";
+      srcCtx.strokeStyle = "rgba(0,0,0,1.0)";
+      return resCtx.strokeStyle = "lightgrey";
+    });
+    return $("#btnLinewidth").change(function() {
+      return srcCtx.lineWidth = resCtx.lineWidth = $(this).val();
+    });
+  };
 
   loadCanvasAndImg = function(id, img) {
     var $id, canvas;
@@ -190,23 +112,25 @@
     $id.empty();
     $id.width(img.width);
     $id.height(img.height);
-    canvas = document.createElement('canvas');
-    canvas.id = "canvasArea";
-    canvas.width = img.width;
-    canvas.height = img.height;
-    canvas.style.zIndex = 1;
-    canvas.style.position = "absolute";
-    canvas.style.border = "1px solid";
-    canvas.style.cursor = "crosshair";
-    canvas.onselectstart = function() {
-      return false;
-    };
-    canvas.onmousedown = function() {
-      return false;
-    };
-    $id.append(canvas);
-    $id.append(img);
-    return initialize("canvasArea");
+    if (Modernizr.canvas) {
+      canvas = document.createElement('canvas');
+      canvas.id = "canvasArea";
+      canvas.width = img.width;
+      canvas.height = img.height;
+      canvas.style.zIndex = 1;
+      canvas.style.position = "absolute";
+      canvas.style.border = "1px solid";
+      canvas.style.cursor = "crosshair";
+      canvas.onselectstart = function() {
+        return false;
+      };
+      canvas.onmousedown = function() {
+        return false;
+      };
+      $id.append(canvas);
+      initialize(canvas);
+    }
+    return $id.append(img);
   };
 
   stopDefault = function(e) {
@@ -219,11 +143,10 @@
     e = e || window.event;
     stopDefault(e);
     files = e.dataTransfer.files;
-    info = "";
     dropBoxElem = e.currentTarget;
     for (i = _i = 0, _len = files.length; _i < _len; i = ++_i) {
       file = files[i];
-      info += 'Name: ' + file.name + '<br/>Size: ' + file.size + ' bytes<br/>';
+      info = 'Name: ' + file.name + '<br/>Size: ' + file.size + ' bytes<br/>';
       reader = new FileReader();
       reader.onload = function(event) {
         var image;
@@ -231,13 +154,13 @@
         image.src = event.target.result;
         return image.onload = (function(image) {
           return function(e) {
-            return loadCanvasAndImg("drop", image);
+            loadCanvasAndImg("drop", image);
+            return $("#imageInfo").html(info);
           };
         })(image);
       };
       reader.readAsDataURL(file);
     }
-    $("#imageInfo").html(info);
     return false;
   };
 
@@ -260,15 +183,8 @@
       addEventHandler(dropBoxElem, 'dragleave', stopDefault);
       return addEventHandler(dropBoxElem, 'drop', drop);
     } else {
-      return $("#drop").html("Please use a HTML5 browser to drag and drop images");
+      return $("#imageInfo").html("Your browser does not support drag and drop of images");
     }
   });
-
-  to_image = function() {
-    var canvas;
-    console.log("drawing");
-    canvas = document.getElementById("drawingCanvas");
-    return document.getElementById("theimage").src = canvas.toDataURL();
-  };
 
 }).call(this);
--- src/documents/scripts/script.js.coffee
+++ src/documents/scripts/script.js.coffee
@@ -3,7 +3,20 @@
 # works out the X, Y position of the click INSIDE the canvas from the X, Y position on 
 # the page
 
-App = App || Globals: {foreGcolour: '#0044cc', backGcolour: '#000000'}
+App = App || Globals: {foreGcolour: $("#btnForeG").css('backgroundColor'), backGcolour: $("#btnBackG").css('backgroundColor')}
+
+#Events
+clearBtn = (resCtx) ->
+  $("#drop").empty()
+  # remember the current line width
+  $("#imageInfo").html ''
+  # clear the content of the canvas 
+  resCtx.save()
+  # Use the identity matrix while clearing the canvas
+  resCtx.setTransform(1, 0, 0, 1, 0, 0)
+  resCtx.clearRect(0, 0, resCtx.canvas.width, resCtx.canvas.height)
+  # Restore the transform
+  resCtx.restore()
 
 getPosition = (mouseEvent, element) ->
   x = undefined
@@ -16,165 +29,82 @@ getPosition = (mouseEvent, element) ->
     y = mouseEvent.clientY + document.body.scrollTop + document.documentElement.scrollTop
   X: x - element.offsetLeft, Y: y - element.offsetTop
 
-canvasDrop2 = (context) ->
-  (e) ->
-    e = e || window.event # get window.event if e argument missing (in IE)  
-    stopDefault e
-    files = e.dataTransfer.files
-    info  = ""
-    formData = new FormData()
-    dropBoxElem = e.currentTarget
-    for file, i in files       
-      info += 'Name: '+file.name+'<br/>Size: '+file.size+' bytes<br/>'
-      formData.append 'file', file
-      reader = new FileReader()
-      document.getElementById("drawingCanvas").width = 2000
-      reader.onload = (event) ->
-        image       = new Image();
-        image.src   = event.target.result
-        #image.width = 250 # a fake resize
-        context.drawImage(image, 0, 0)
-      reader.readAsDataURL file
-    $("#imageInfo").html info
-    false
+# draws a line to the x and y coordinates of the mouse event inside
+# the specified element using the specified context
+drawLine  = (context, position) ->
+  context.lineTo position.X, position.Y
+  context.stroke()
+#draw lines on the src and result canvas
+drawLines = (srcCtx, resCtx, position) ->
+  drawLine srcCtx, position
+  drawLine resCtx, position
 
-canvasDrop = (context) ->
-  (e) ->
-    e = e || window.event
-    stopDefault e
-    files = e.dataTransfer.files
-    info  = ""
-    for file, i in files
-      info += 'Name: '+file.name+'<br/>Size: '+file.size+' bytes<br/>'
-      image = new Image()
-      image.onload = ((image, i) ->
-        (e) ->
-          # Size adjustment 
-          canvas  = document.getElementById("drawingCanvas")
-          canvas.width = image.width
-          canvas.height = image.height
-          context = canvas.getContext("2d")
-          context.drawImage image, 0, 0
-  #(canvas.height - image.height) / 2, 2, image.width, image.height
-          window.URL.revokeObjectURL @src
-      )(image, i)
-      image.src = window.URL.createObjectURL file
-      context.drawImage(image, 0, 0)
-      image = null
-    $("#imageInfo").html info
-    false
+# finish drawing on the src and result canvas given the end position
+finishDrawings = (srcCtx, resCtx, position) ->
+  # draw the line to the finishing coordinates
+  drawLines srcCtx, resCtx, position
+  srcCtx.closePath() 
+  resCtx.closePath()
+  # unbind any events which could draw
+  $(srcCtx.canvas).unbind("mousemove").unbind("mouseup").unbind "mouseout"
 
-
-initialize = (canvasId)->
+initialize = (srcCanvas)->
   # get references to the canvas element as well as the 2D drawing context
-  element             = document.getElementById(canvasId)
-  context             = element.getContext("2d")
-  context.strokeStyle = App.Globals.foreGcolour
-  context.lineWidth   = $("#btnLinewidth").val()
-  context.lineJoin    = "round"
-  context.lineCap     = "round"
+  srcCtx             = srcCanvas.getContext "2d"
+  srcCtx.strokeStyle = App.Globals.foreGcolour
 
-  if canvasId == "drawingCanvas"
-    element.addEventListener 'dragenter', stopDefault, false
-    element.addEventListener 'dragover' , stopDefault, false
-    element.addEventListener 'drop'     , canvasDrop(context), false
+  resCanvas          = document.getElementById "resultCanvas"
+  resCtx             = resCanvas.getContext "2d"
+  resCanvas.width    = srcCanvas.width
+  resCanvas.height   = srcCanvas.height
 
-  element2 = document.getElementById("resultCanvas") 
-  context2 = element2.getContext("2d")
-  element2.width       = element.width
-  element2.height       = element.height
-  context2.strokeStyle = "white"
-  context2.lineWidth   = $("#btnLinewidth").val()
-  context2.lineJoin    = "round"
-  context2.lineCap     = "round"
-  context2.fillStyle   = "lightgrey";
-  context2.fillRect(0, 0, element.width, element.height);
+  srcCtx.lineWidth   = resCtx.lineWidth = $("#btnLinewidth").val()
+  srcCtx.lineJoin    = resCtx.lineJoin  = "round"
+  srcCtx.lineCap     = resCtx.lineCap   = "round"
+
+  resCtx.strokeStyle = "white"
+  resCtx.fillStyle   = "lightgrey"
+  resCtx.fillRect 0, 0, srcCanvas.width, srcCanvas.height
 
   # start drawing when the mousedown event fires, and attach handlers to 
   # draw a line to wherever the mouse moves to
-  $(element).mousedown (mouseEvent) ->
-    position = getPosition(mouseEvent, element)
-    context.moveTo position.X, position.Y
-    context.beginPath()
-    context2.moveTo position.X, position.Y
-    context2.beginPath()
+  $(srcCanvas).mousedown (mouseEvent) ->
+    position = getPosition(mouseEvent, srcCanvas)
+    srcCtx.moveTo position.X, position.Y
+    srcCtx.beginPath()
+    resCtx.moveTo position.X, position.Y
+    resCtx.beginPath()
     # attach event handlers
     $(this).mousemove((mouseEvent) ->
-      position = getPosition(mouseEvent, element)
-      drawLine mouseEvent, element, context, position
-      drawLine mouseEvent, element2, context2, position
+      position = getPosition(mouseEvent, srcCanvas)
+      drawLines srcCtx, resCtx, position
     ).mouseup((mouseEvent) ->
-      position = getPosition(mouseEvent, element)
-      drawLine mouseEvent, element, context, position
-      drawLine mouseEvent, element2, context2, position
-      finishDrawing mouseEvent, element, context
-      finishDrawing mouseEvent, element2, context2
+      position = getPosition(mouseEvent, srcCanvas)
+      finishDrawings srcCtx, resCtx, position
     ).mouseout (mouseEvent) ->
-      position = getPosition(mouseEvent, element)
-      drawLine mouseEvent, element, context, position
-      drawLine mouseEvent, element2, context2, position
-      finishDrawing mouseEvent, element, context
-      finishDrawing mouseEvent, element2, context2
+      position = getPosition(mouseEvent, srcCanvas)
+      finishDrawings srcCtx, resCtx, position
 
   #Clear
-  $("#btnClear").click ->
-    # remember the current line width
-    currentWidth      = context.lineWidth
-    element.width     = element.width
-    context.lineWidth = currentWidth
-    $("#imageInfo").html ''
-    # clear the content of the canvas 
-    resCtx = document.getElementById("resultCanvas").getContext("2d")
-    resCtx.save();
-    # Use the identity matrix while clearing the canvas
-    resCtx.setTransform(1, 0, 0, 1, 0, 0);
-    resCtx.clearRect(0, 0, resCtx.canvas.width, resCtx.canvas.height);
-    # Restore the transform
-    resCtx.restore();
-  
+  $("#btnClear").click -> clearBtn(resCtx)
+
   #Change the line colour depending on fore/background
   $("#btnForeG").click ->
-    context.globalCompositeOperation  = "source-over"
-    context.strokeStyle  = App.Globals.foreGcolour
-    context2.strokeStyle = "white"
+    srcCtx.globalCompositeOperation = "source-over"
+    srcCtx.strokeStyle = App.Globals.foreGcolour
+    resCtx.strokeStyle = "white"
   $("#btnBackG").click ->
-    context.globalCompositeOperation  = "source-over"
-    context.strokeStyle  = App.Globals.backGcolour
-    context2.strokeStyle = "black"
+    srcCtx.globalCompositeOperation = "source-over"
+    srcCtx.strokeStyle = App.Globals.backGcolour
+    resCtx.strokeStyle = "black"
   $("#btnEraser").click ->
-    context.globalCompositeOperation  = "destination-out"
-    context.strokeStyle               = "rgba(0,0,0,1.0)"
-    context2.strokeStyle              = "lightgrey"
+    srcCtx.globalCompositeOperation = "destination-out"
+    srcCtx.strokeStyle              = "rgba(0,0,0,1.0)"
+    resCtx.strokeStyle              = "lightgrey"
 
   # change the line width
-  $("#btnLinewidth").change (event) ->
-    context.lineWidth = $(this).val()
-    context2.lineWidth = $(this).val()
-
-# draws a line to the x and y coordinates of the mouse event inside
-# the specified element using the specified context
-drawLine   = (mouseEvent, element, context, position) ->
-  #position = getPosition(mouseEvent, element)
-  context.lineTo position.X, position.Y
-  context.stroke()
-
-# draws a line from the last coordiantes in the path to the finishing
-# coordinates and unbind any event handlers which need to be preceded
-# by the mouse down event
-finishDrawing = (mouseEvent, element, context) ->
-  # draw the line to the finishing coordinates
-  #drawLine mouseEvent, element, context
-  context.closePath()
-  
-  # unbind any events which could draw
-  $(element).unbind("mousemove").unbind("mouseup").unbind "mouseout"
-
-$(document).ready ->
-  unless Modernizr.canvas
-    $("#message").html "<p><b>WARNING</b>: Your browser does not support HTML5's canvas feature, you won't be able to see the drawings below</p>"
-    $("article").hide()
-  else
-    #initialize("drawingCanvas")
+  $("#btnLinewidth").change () ->
+    srcCtx.lineWidth = resCtx.lineWidth = $(this).val()
 
 #Drag and drop events
 loadCanvasAndImg = (id, img) ->
@@ -182,19 +112,20 @@ loadCanvasAndImg = (id, img) ->
   $id.empty()
   $id.width  img.width
   $id.height img.height
-  canvas                = document.createElement 'canvas'
-  canvas.id             = "canvasArea"
-  canvas.width          = img.width
-  canvas.height         = img.height
-  canvas.style.zIndex   = 1
-  canvas.style.position = "absolute"
-  canvas.style.border   = "1px solid"
-  canvas.style.cursor   = "crosshair"
-  canvas.onselectstart  = () -> false # IE: disable text selection
-  canvas.onmousedown    = () -> false # mozilla: disable text selection
-  $id.append canvas
+  if Modernizr.canvas
+    canvas                = document.createElement 'canvas'
+    canvas.id             = "canvasArea"
+    canvas.width          = img.width
+    canvas.height         = img.height
+    canvas.style.zIndex   = 1
+    canvas.style.position = "absolute"
+    canvas.style.border   = "1px solid"
+    canvas.style.cursor   = "crosshair"
+    canvas.onselectstart  = () -> false # IE: disable text selection
+    canvas.onmousedown    = () -> false # mozilla: disable text selection
+    $id.append canvas
+    initialize canvas
   $id.append img
-  initialize "canvasArea"
 
 stopDefault = (e) ->
   e.stopPropagation()
@@ -204,23 +135,19 @@ drop = (e) ->
   e = e || window.event; # get window.event if e argument missing (in IE)  
   stopDefault e
   files = e.dataTransfer.files
-  info  = ""
-  #formData = new FormData()
   dropBoxElem = e.currentTarget
   for file, i in files       
-    info += 'Name: '+file.name+'<br/>Size: '+file.size+' bytes<br/>'
-    #formData.append 'file', file
+    info   = 'Name: '+file.name+'<br/>Size: '+file.size+' bytes<br/>'
     reader = new FileReader()
     reader.onload = (event) ->
-      image       = new Image();
+      image       = new Image()
       image.src   = event.target.result
       image.onload = ((image) -> 
         (e) ->
           loadCanvasAndImg "drop", image
+          $("#imageInfo").html info
       )(image)
     reader.readAsDataURL file
-  #e.currentTarget.innerHTML = info
-  $("#imageInfo").html info
   false
 
 # cross-browser addEventHandler()  
@@ -240,9 +167,4 @@ $(document).ready ->
     addEventHandler dropBoxElem, 'dragleave', stopDefault
     addEventHandler dropBoxElem, 'drop'     , drop
   else
-    $("#drop").html "Please use a HTML5 browser to drag and drop images"
-
-to_image = ->
-  console.log "drawing"
-  canvas = document.getElementById("drawingCanvas")
-  document.getElementById("theimage").src = canvas.toDataURL();
+    $("#imageInfo").html "Your browser does not support drag and drop of images"
